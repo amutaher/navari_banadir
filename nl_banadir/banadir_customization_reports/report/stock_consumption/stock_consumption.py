6695cb007bc0af725ba4267bfccdb2d8c5cf1ee8
emmanuel-mwendwa <mwendwae054@gmail.com>
2025-03-10 11:37:51 +0300

    feat: add filters functionality to the report
    TODO:// Split up the data into date ranges [30, 60, 90]

--- nl_banadir/banadir_customization_reports/report/stock_consumption/stock_consumption.py
+++ nl_banadir/banadir_customization_reports/report/stock_consumption/stock_consumption.py
@@ -6,16 +6,13 @@ from frappe.utils import get_datetime, flt
 
 
 def execute(filters=None):
-	columns = get_columns()
+    columns = get_columns()
 
-	tracker = ConsumptionTracker(filters)
-	consumption_data = tracker.generate()
+    data = ConsumptionTracker(filters).generate()
 
-	data = format_data(consumption_data)
+    data = format_data(data)
 
-	# frappe.throw(str(data))
-
-	return columns, data
+    return columns, data
 
 
 def get_columns():
@@ -54,82 +51,143 @@ def format_data(consumption_data):
     return data
 
 class ConsumptionTracker:
-	"""Tracks how long it takes to completely consume a stock item from when it was purchased or received"""
+    """Tracks how long it takes to completely consume a stock item from when it was purchased or received"""
 
-	def __init__(self, filters: dict | None = None, sle: list | None = None):
-		self.item_details = {}
-		self.filters = filters
-		self.sle = sle
+    def __init__(self, filters: dict | None = None, sle: list | None = None):
+        self.item_details = {}
+        self.filters = filters
+        self.sle = sle
 
-	def generate(self) -> dict:
-		"""
-			Returns a dictionary structured as:
-			Key = (Item, Warehouse)
-			Value = List of consumption durations for different batches of stock
-		"""
+    def generate(self) -> dict:
+        """
+            Returns a dictionary structured as:
+            Key = (Item, Warehouse)
+            Value = List of consumption durations for different batches of stock
+        """
 
-		stock_ledger_entries = self.sle if self.sle else self.__get_stock_ledger_entries()
+        stock_ledger_entries = self.sle if self.sle else self.__get_stock_ledger_entries()
 
-		for entry in stock_ledger_entries:
-			key, fifo_queue = self.__init_key_store(entry)
+        for entry in stock_ledger_entries:
+            key, fifo_queue = self.__init_key_store(entry)
 
-			if entry.actual_qty > 0:
-				self.__record_incoming_stock(entry, fifo_queue)
-			else:
-				self.record_outgoing_stock(entry, fifo_queue)
+            if entry.actual_qty > 0:
+                self.__record_incoming_stock(entry, fifo_queue)
+            else:
+                self.record_outgoing_stock(entry, fifo_queue)
 
-		return self.item_details
-	
-	def __init_key_store(self, row: dict) -> tuple:
-		"""Initialize the FIFO queue for each item and warehouse"""
-		key = (row.item_code, row.warehouse)
-		self.item_details.setdefault(key, {"fifo_queue": [], "consumption_times": []})
-		fifo_queue = self.item_details[key]["fifo_queue"]
+        return self.item_details
+    
+    def __init_key_store(self, row: dict) -> tuple:
+        """Initialize the FIFO queue for each item and warehouse"""
+        key = (row.item_code, row.warehouse)
+        self.item_details.setdefault(key, {"fifo_queue": [], "consumption_times": []})
+        fifo_queue = self.item_details[key]["fifo_queue"]
 
-		return key, fifo_queue
-	
-	def __record_incoming_stock(self, row: dict, fifo_queue: list):
-		"""Record stock received with its date."""
-		fifo_queue.append([row.actual_qty, row.posting_date])
+        return key, fifo_queue
+    
+    def __record_incoming_stock(self, row: dict, fifo_queue: list):
+        """Record stock received with its date."""
+        fifo_queue.append([row.actual_qty, row.posting_date])
 
-	def record_outgoing_stock(self, row: dict, fifo_queue: list):
-		"""Track stock consumption and calculate consumption time."""
-		if not fifo_queue:
-			return
-		
-		qty_to_consume = abs(row.actual_qty)
-		key = (row.item_code, row.warehouse)
+    def record_outgoing_stock(self, row: dict, fifo_queue: list):
+        """Track stock consumption and calculate consumption time."""
+        if not fifo_queue:
+            return
+        
+        qty_to_consume = abs(row.actual_qty)
+        key = (row.item_code, row.warehouse)
 
-		while qty_to_consume > 0 and fifo_queue:
-			first_entry = fifo_queue[0]
+        while qty_to_consume > 0 and fifo_queue:
+            first_entry = fifo_queue[0]
 
-			if first_entry[0] <= qty_to_consume:
-				# If full batch is consumed, track duration
-				consumption_time = (row.posting_date - first_entry[1]).days
-				self.item_details[key]["consumption_times"].append(consumption_time)
-				qty_to_consume -= first_entry[0]
-				fifo_queue.pop(0)
-			else:
-				# If batch is partially consumed, update the batch quantity
-				first_entry[0] -= qty_to_consume
-				qty_to_consume = 0
+            if first_entry[0] <= qty_to_consume:
+                # If full batch is consumed, track duration
+                consumption_time = (row.posting_date - first_entry[1]).days
+                self.item_details[key]["consumption_times"].append(consumption_time)
+                qty_to_consume -= first_entry[0]
+                fifo_queue.pop(0)
+            else:
+                # If batch is partially consumed, update the batch quantity
+                first_entry[0] -= qty_to_consume
+                qty_to_consume = 0
 
-	def __get_stock_ledger_entries(self) -> list:
-		"""Get stock ledger entries based on filters"""
-		sle = frappe.qb.DocType("Stock Ledger Entry")
-		return(
-			frappe.qb.from_(sle)
-			.select(
-				sle.name,
-				sle.item_code,
-				sle.warehouse,
-				sle.actual_qty,
-				sle.posting_date
-			)
-			.where(
-				(sle.company == self.filters.get("company"))
-				& (sle.is_cancelled != 1)
-			)
-			.orderby(sle.posting_date)
-			.run(as_dict=True)		
-		)
+    def __get_stock_ledger_entries(self) -> list:
+        sle = frappe.qb.DocType("Stock Ledger Entry")
+        item = self.__get_item_query()  # used as derived table in sle query
+        to_date = get_datetime(self.filters.get("to_date") + " 23:59:59")
+
+        sle_query = (
+            frappe.qb.from_(sle)
+            .from_(item)
+            .select(
+                sle.item_code,
+                item.name,
+                item.item_name,
+                item.item_group,
+                item.brand,
+                item.description,
+                item.stock_uom,
+                item.has_serial_no,
+                sle.actual_qty,
+                sle.stock_value_difference,
+                sle.posting_date,
+                sle.voucher_type,
+                sle.voucher_no,
+                sle.serial_no,
+                sle.batch_no,
+                sle.qty_after_transaction,
+                sle.serial_and_batch_bundle,
+                sle.warehouse,
+            )
+            .where(
+                (sle.item_code == item.name)
+                & (sle.company == self.filters.get("company"))
+                & (sle.posting_datetime <= to_date)
+                & (sle.is_cancelled != 1)
+            )
+        )
+
+        if self.filters.get("warehouse"):
+            sle_query = self.__get_warehouse_conditions(sle, sle_query)
+        elif self.filters.get("warehouse_type"):
+            warehouses = frappe.get_all(
+                "Warehouse",
+                filters={"warehouse_type": self.filters.get("warehouse_type"), "is_group": 0},
+                pluck="name",
+            )
+
+            if warehouses:
+                sle_query = sle_query.where(sle.warehouse.isin(warehouses))
+
+        sle_query = sle_query.orderby(sle.posting_datetime, sle.creation)
+
+        return sle_query.run(as_dict=True)
+    
+    def __get_item_query(self) -> str:
+        item_table = frappe.qb.DocType("Item")
+
+        item = frappe.qb.from_("Item").select(
+            "name", "item_name", "description", "stock_uom", "brand", "item_group", "has_serial_no"
+        )
+
+        if self.filters.get("item_code"):
+            item = item.where(item_table.item_code == self.filters.get("item_code"))
+
+        if self.filters.get("brand"):
+            item = item.where(item_table.brand == self.filters.get("brand"))
+
+        return item
+    
+    def __get_warehouse_conditions(self, sle, sle_query) -> str:
+        warehouse = frappe.qb.DocType("Warehouse")
+        lft, rgt = frappe.db.get_value("Warehouse", self.filters.get("warehouse"), ["lft", "rgt"])
+
+        warehouse_results = (
+            frappe.qb.from_(warehouse)
+            .select("name")
+            .where((warehouse.lft >= lft) & (warehouse.rgt <= rgt))
+            .run()
+        )
+        warehouse_results = [x[0] for x in warehouse_results]
+
+        return sle_query.where(sle.warehouse.isin(warehouse_results))
