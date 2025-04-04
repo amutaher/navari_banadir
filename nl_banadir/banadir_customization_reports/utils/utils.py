3d4dc8f3a54413196f16c3873866a11e8c9d10eb
maniamartial <martialmania19@gmail.com>
2025-04-04 09:54:16 +0300

    feat - Limit choosing of branch that doesnt have company

--- nl_banadir/banadir_customization_reports/utils/utils.py
+++ nl_banadir/banadir_customization_reports/utils/utils.py
@@ -15,6 +15,10 @@ def validate_branch_company(doc, method):
             _("Branch <b>{0}</B> belongs to company <b>{1}</b>, but you are using company <b>{2}.</b>")
             .format(doc.branch, branch_company, doc.company)
         )
+    if not branch_company:
+        frappe.throw(
+            _("Branch <b>{0}</B> does not belong to any company.").format(doc.branch)
+        )
 
 def validate_branch_company_item_level(doc , method):
     """
@@ -23,11 +27,18 @@ def validate_branch_company_item_level(doc , method):
     """
 
     for item in doc.accounts:
+        if not item.branch:
+            continue
         item_branch_company = frappe.get_value("Branch", item.branch, "custom_company")
 
         if item_branch_company and doc.company and item_branch_company != doc.company:
             frappe.throw(
                 _("Branch <b>{0}</B> belongs to company <b>{1}</b>, but you are using company <b>{2}.</b>")
                 .format(item.branch, item_branch_company, doc.company)
+            )
+            break
+        if not item_branch_company:
+            frappe.throw(
+                _("Branch <b>{0}</B> does not belong to any company.").format(item.branch)
             )
             break
