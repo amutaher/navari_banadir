8aef9995a2ec905a4d285c9dd680493b8f90520e
maniamartial <martialmania19@gmail.com>
2025-04-25 11:00:16 +0300

    feat - Disallow updating the fields once you've submitted the invoice

--- nl_banadir/banadir_customization_reports/controllers/work_order.py
+++ nl_banadir/banadir_customization_reports/controllers/work_order.py
@@ -103,8 +103,6 @@ def create_purchase_invoice(doc, operation, company, currency, custom_work_order
         },
     )
 
-    # frappe.throw(str(purchase_invoice.items))
-
     purchase_invoice.insert()
     # validate_accounts(purchase_invoice)
     purchase_invoice.submit()
@@ -140,6 +138,7 @@ def on_update(doc, method=None):
     """
     Main function to handle the creation of Purchase Invoices for completed operations.
     """
+    validate_invoice_created(doc)
     validate_operations(doc)
     validate_operations_seq(doc)
     for operation in doc.custom_subcontractors:
@@ -343,3 +342,41 @@ def update_is_finished(doc):
         return True
     else:
         return False
+
+
+def validate_invoice_created(doc):
+    """Validate that invoiced rows aren't modified"""
+    set_read_only_for_invoiced_subcontractors(doc)
+
+
+def set_read_only_for_invoiced_subcontractors(doc):
+    """Block modifications only if invoiced rows are changed"""
+    if not doc.get("custom_subcontractors"):
+        return
+
+    doc_before_save = doc.get_doc_before_save()
+
+    if not doc_before_save:
+        return
+
+    old_rows = {
+        row.name: row for row in doc_before_save.get("custom_subcontractors", [])
+    }
+
+    for row in doc.get("custom_subcontractors", []):
+        if not row.get("invoice_created") == 1:
+            continue
+
+        if row.name in old_rows:
+            old_row = old_rows[row.name]
+            for field in ["status", "item", "rate", "supplier"]:
+                if row.get(field) != old_row.get(field):
+                    frappe.throw(
+                        title="Invoice Exists - Modification Blocked",
+                        msg=f"""
+                        Cannot modify operation <strong>{row.get("operations")}</strong>
+                        because an invoice already exists.
+                        <br><br>
+                        Field changed: <strong>{frappe.unscrub(field)}</strong>
+                        """,
+                    )
