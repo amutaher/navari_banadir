bf49b509a31d2b1a1f5f28bf4ff3c4defac8f744
maniamartial <martialmania19@gmail.com>
2025-05-16 10:27:02 +0300

    chore: Avoid code repeatation

--- nl_banadir/public/js/asset.js
+++ nl_banadir/public/js/asset.js
@@ -1,53 +1,18 @@
 frappe.ui.form.on("Asset", {
   custom_multi_currency: function (frm) {
-    frappe.db.get_value("Company", frm.doc.company, "default_currency", (r) => {
-      if (r && r.default_currency) {
-        console.log("Default Currency: ", r.default_currency);
-        // You can use r.default_currency to set a field or for other logic
-        frm.set_value("custom_company_currency", r.default_currency);
-        frm.refresh_fields("custom_company_currency");
-      } else {
-        console.log("Default Currency not found");
-      }
-    });
-    const toCurrency = frm.doc.custom_company_currency;
-
-    if (toCurrency) {
-      frappe.db.get_value(
-        "Currency Exchange",
-        {
-          from_currency: frm.doc.custom_transaction_currency,
-          to_currency: toCurrency,
-        },
-        ["exchange_rate"],
-        (response) => {
-          frm.doc.custom_transaction_exchange_rate = response.exchange_rate;
-          frm.refresh_fields("custom_transaction_exchange_rate");
-        },
-      );
-    }
+    updateCompanyCurrencyAndExchangeRate(frm);
   },
-  custom_company_currency: function (frm) {
-    const toCurrency = frm.doc.custom_company_currency;
 
-    if (toCurrency) {
-      frappe.db.get_value(
-        "Currency Exchange",
-        {
-          from_currency: frm.doc.custom_transaction_currency,
-          to_currency: toCurrency,
-        },
-        ["exchange_rate"],
-        (response) => {
-          frm.doc.custom_transaction_exchange_rate = response.exchange_rate;
-          frm.refresh_fields("custom_transaction_exchange_rate");
-        },
-      );
-    }
+  custom_transaction_currency: function (frm) {
+    updateCompanyCurrencyAndExchangeRate(frm);
+  },
+
+  custom_company_currency: function (frm) {
+    updateExchangeRate(frm);
   },
 
   custom_net_purchase_amounttransaction_currency: function (frm) {
-    exchange_rate = frm.doc.custom_transaction_exchange_rate;
+    const exchange_rate = frm.doc.custom_transaction_exchange_rate;
     if (exchange_rate) {
       frm.doc.gross_purchase_amount =
         frm.doc.custom_net_purchase_amounttransaction_currency * exchange_rate;
@@ -55,3 +20,38 @@ frappe.ui.form.on("Asset", {
     }
   },
 });
+
+// --- Helper Functions ---
+
+function updateCompanyCurrencyAndExchangeRate(frm) {
+  frappe.db.get_value("Company", frm.doc.company, "default_currency", (r) => {
+    if (r && r.default_currency) {
+      console.log("Default Currency: ", r.default_currency);
+      frm.set_value("custom_company_currency", r.default_currency);
+      frm.refresh_fields("custom_company_currency");
+      updateExchangeRate(frm);
+    } else {
+      console.log("Default Currency not found");
+    }
+  });
+}
+
+function updateExchangeRate(frm) {
+  const toCurrency = frm.doc.custom_company_currency;
+  const fromCurrency = frm.doc.custom_transaction_currency;
+
+  if (fromCurrency && toCurrency) {
+    frappe.db.get_value(
+      "Currency Exchange",
+      {
+        from_currency: fromCurrency,
+        to_currency: toCurrency,
+      },
+      ["exchange_rate"],
+      (response) => {
+        frm.doc.custom_transaction_exchange_rate = response.exchange_rate;
+        frm.refresh_fields("custom_transaction_exchange_rate");
+      },
+    );
+  }
+}
